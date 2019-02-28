bda8832f8ea424cd103149b009beb05c978804fe
Umar Khan <umarkhandev@gmail.com>
2019-02-28 23:00:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(string)
-  string = string.split(" ")
-  string.collect do |words|
-    words.reverse!
-  end
+ string = string.split(" ")
+     arr = []
+     string.each do |names|
+     names.reverse!
+     arr.push("#{names}")
+   end
+   string = arr.join(" ")
 end
